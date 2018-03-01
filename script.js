ecf3e0c2da2adf06ddad58e995cd7bbbaa7f852b
Edmund <edmundcjj@gmail.com>
2018-03-02 00:25:24 +0800

    Can print board with values input by user

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Initializa tic-tac-toe board with default values
 var board = {
   top:{
     col1:".",
@@ -16,30 +18,34 @@ var board = {
   }
 };
 
-var boardOutput = "";
+// Function to print the board
+function print_Board(board){
+  var boardOutput = "";
 
-// loop through each row
-for( var rowKey in board ){
+  // loop through each row
+  for( var rowKey in board ){
 
-  /*
-   * make a variable for convenience
-   * a shortcut so you won't have
-   * to write board[rowKey][columnKey]
-   */
-  var row = board[rowKey];
+    /*
+     * make a variable for convenience
+     * a shortcut so you won't have
+     * to write board[rowKey][columnKey]
+     */
+    var row = board[rowKey];
 
-  // loop through each column
-  for( var columnKey in row ){
+    // loop through each column
+    for( var columnKey in row ){
 
-    // concatenate the string together
-    boardOutput = boardOutput + row[columnKey];
+      // concatenate the string together
+      boardOutput = boardOutput + row[columnKey];
+    }
+
+    // make a newline so that each row begins on a new line
+    boardOutput = boardOutput + "\n";
   }
 
-  // make a newline so that each row begins on a new line
-  boardOutput = boardOutput + "\n";
+  console.log( boardOutput );
 }
 
-console.log( boardOutput );
 
 // set a variable that represents
 // whether or not the game is currently running
@@ -47,10 +53,21 @@ var running = true;
 
 // run the game on a loop
 while( running ){
-  var row = prompt("enter your row: top, middle or bottom");
-  var column = prompt("enter your column: col1, col2, col3");
+  // Ask user for input at which row and column
+  var row = prompt("Enter your row: top, middle or bottom");
+  var column = prompt("Enter your column: col1, col2, col3");
 
-  console.log("current value @: ", board[row][column] );
+  // Ask user for the value that is to be inserted
+  var value = prompt("Enter your value: X, O");
+
+  // Update board with value user inserted
+  board[row][column] = value;
+
+  // Display location of where the value is & and the value itselfS
+  console.log("Current value @ " + "[" + row + "]" + "[" + column + "] = ", board[row][column]);
+
+  // Display the board with the updated value
+  print_Board(board);
 
   // you can also use the break statement to get out of a while loop
   break;
