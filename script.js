b76b31f332d2813c2ad09a7aceabf412381362df
stonefruit <jeromeng888@gmail.com>
2018-03-01 15:54:24 +0800

    add checker for position. complete working version

--- script.js
+++ script.js
@@ -19,62 +19,83 @@ var board = {
 // copy of empty board for resetting board
 var emptyBoard = JSON.parse(JSON.stringify(board)); 
 
-var boardOutput = "";
-
 // loop through each row
-for( var rowKey in board ){
+function printBoard(board) {
+  var boardOutput = "";
+  for( var rowKey in board ){
 
-  /*
-   * make a variable for convenience
-   * a shortcut so you won't have
-   * to write board[rowKey][columnKey]
-   */
-  var row = board[rowKey];
+    /*
+    * make a variable for convenience
+    * a shortcut so you won't have
+    * to write board[rowKey][columnKey]
+    */
+    var row = board[rowKey];
 
-  // loop through each column
-  for( var columnKey in row ){
+    // loop through each column
+    for( var columnKey in row ){
 
-    // concatenate the string together
-    boardOutput = boardOutput + row[columnKey];
+      // concatenate the string together
+      boardOutput = boardOutput + row[columnKey];
+    }
+
+    // make a newline so that each row begins on a new line
+    boardOutput = boardOutput + "\n";
   }
 
-  // make a newline so that each row begins on a new line
-  boardOutput = boardOutput + "\n";
+  console.log( boardOutput );
+
 }
 
-console.log( boardOutput );
+printBoard(board);
 
 // set a variable that represents
 // whether or not the game is currently running
 var running = true;
 
+var player = 1;
 // run the game on a loop
 while( running ){
-  var row = prompt("enter your row: top, middle or bottom");
-  var column = prompt("enter your column: col1, col2, col3");
-
-  console.log("current value @: ", board[row][column] );
-
-  // if game is won, end game
-  // if (board.top.col1 === board.top.col2 && board.top.col2 === board.top.col3 && board.top.col1 !== ".") {
-  //   alert("The winner is " + board.top.col1 + "!");
-  // }
-  // else if (board.middle.col1 === board.middle.col2 && board.middle.col2 === board.middle.col3 && board.middle.col1 !== ".") {
-  //   alert("The winner is " + board.middle.col1 + "!");
-  // }
-
-  if (winnerChecker !== false) {
-    alert("The winner is " + winnerChecker + "!");
-    break;
+  var positionCheck = true;
+  while (positionCheck) {
+    var row = prompt("enter your row: top, middle or bottom");
+    var column = prompt("enter your column: col1, col2, col3");
+    if (board[row][column] !== ".") {
+      alert("current position is taken. Please try another position." );
+    }
+    else {
+      board[row][column] = player;
+      console.log("current value @: ", board[row][column] );
+      positionCheck = false;
+    }
   }
 
-  // if all spaces are filled, end game
+  printBoard(board);
+
+  // if there is a winner, end game and print winner
+  if (winnerChecker(board) !== false) {
+    alert("The winner is " + winnerChecker(board) + "!");
+    break;
+  }
+  
+  // if all spaces are filled, end game and print alert
+  var fillCount = 0;
   for (row in board) {
     for (col in row) {
       if (board[row][col] !== ".") {
-        break;
+        fillCount++;
       }
     }
+  }
+  if (fillCount === 9) {
+    alert("There is no winner!");
+  }
+
+  // toggle player
+  if (player === 1) {
+    player = 2;
+  }
+  else {
+    player = 1;
   }
 }
 
@@ -101,7 +122,7 @@ function winnerChecker(board) {
     return board.top.col1;
   }
   else if (board.top.col3 === board.middle.col2 && board.middle.col2 === board.bottom.col1 && board.top.col3 !== ".") {
-    return board.top.col1;
+    return board.top.col3;
   }
   else {
     return false;
