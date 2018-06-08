ede9c21681c4836f284f05bc7c28cbdeba2afde8
scott88lee <scott88lee@gmail.com>
2018-06-08 17:35:07 +0800

    Complete remake, dynamic board and winstate

--- script.js
+++ script.js
@@ -1,103 +1,125 @@
-var board = {
-  top:{
-    col1:".",
-    col2:".",
-    col3:"."
-  },
-  middle:{
-    col1:".",
-    col2:".",
-    col3:"."
-  },
-  bottom:{
-    col1:".",
-    col2:".",
-    col3:"."
-  }
-};
-
-var boardOutput = "";
+var occupiedCount = 0;
 var firstPlayerTurn = true;
+var board = [];
+var boardSize = 0;
+var winString1 = "";
+var winString2 = "";
 
+function generateBoard(size) {  //Dynamically generate board
+  for (var i=0; i<size; i++) {
+    board.push([]);             //Generate row score
 
-function updateBoard() {   //loop and printout board
-  for( var rowKey in board ){
-    var row = board[rowKey];
-      for( var columnKey in row ){
-        boardOutput = boardOutput + row[columnKey] + " ";
-      }
-    boardOutput = boardOutput + "\n";    
-  }
-  console.log( boardOutput + "\n\n");
-}
+    winString1 = winString1 + "X";  //dynamic checkStr
+    winString2 = winString2 + "O";
 
+    for (var k=0; k<size; k++) {
 
-function checkWin(){
-
-  for ( var rowKey in board ){  //check horizontal
-    if (board[rowKey]["col1"] != ".") {
-      if (board[rowKey]["col1"] === board[rowKey]["col2"] && board[rowKey]["col2"] === board[rowKey]["col3"]) {
-        state = false;
-        alert("WIN!!!!!!");
-      }
-    }
-  }
-  //check vertical to be done
-  //check diagonal to be dowe
-}
-
-  //CCount number of "."
-function checkBoardFull(){
-  var boardSpaces = 0;
-  for( var rowKey in board ){
-    row = board[rowKey];
-      for( var columnKey in row ){
-        if (row[columnKey] === "."){
-          boardSpaces++;
-        } 
-      }
-    }
-    if (boardSpaces === 0) {
-      state = false;
-      alert("Its a draw!");
+      //Button factory
+      var addButton = document.createElement('div');
       
+      addButton.row = i;        //addButton is an obj
+      addButton.col = k;        //you can manipulate it as such
+      board[i].push(" ");       //assign id and scores
+
+
+      addButton.style.display = "inline-block";
+      addButton.style.border = "2px solid black";
+      addButton.style.height = "100px";
+      addButton.style.width = "100px";
+      document.body.appendChild(addButton);
+
+      //Adds listeners to every button
+      addButton.addEventListener('click', game);
     }
+    var br = document.createElement('br');
+    document.body.appendChild(br);
+    //Creates break for the next row
+  }
+  boardSize = size;
 }
 
-//Prompt Player 1 2 input
-function playerInput() {
-  var dupe = false;
-  if (firstPlayerTurn === true){
-      currentPlayer = "Player 1 - "
-      symbol = "X";
+function game() { 
+  //Draws the X's and O's
+  if ( checkEmpty(this) ) {
+    ++occupiedCount;
+    this.textContent = draw(this); // Draw functions decides X or O;
+                // 
+    checkWinState();
+  } else {
+
+    //Space taken
+    console.log('Piss Off!');
+  }
+}
+
+function checkEmpty(box) {
+  if (box.textContent == "X" || box.textContent == "O") {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function draw(box) {
+  if (firstPlayerTurn == true) {
+    firstPlayerTurn = !firstPlayerTurn;
+    board[box.row][box.col] = "X";
+    return "X";
+  } else {
+    firstPlayerTurn = !firstPlayerTurn;
+    board[box.row][box.col] = "O";
+    return "O";
+  }
+}
+
+function checkWinState() {
+  // HORIZONTAL CHECK
+  var checkString = "";
+  for (var i=0; i<board.length; i++) {
+    for (var k=0; k<board[i].length; k++) {
+      checkString = checkString + board[i][k];
+    } 
+    if (checkString == winString1 || checkString == winString2) {
+      alert('WIN!!!');
     } else {
-      currentPlayer = "Player 2 - "
-      symbol = "O";
+      checkString = "";
     }
-  do {
-    var row = prompt( currentPlayer + "Enter row: top, middle or bottom");
-    var column = prompt( currentPlayer + "Enter column: col1, col2, col3");
+  }
 
-    if (board[row][column] === "."){
-      board[row][column] = symbol;
-      firstPlayerTurn = !firstPlayerTurn;
-      updateBoard();
-      dupe = false;
-
+  //VERTICAL CHECK
+  for (var i=0; i<board.length; i++) {
+    for (var k=0; k<board[i].length; k++) {
+      checkString = checkString + board[k][i];  //reverse k & i
+    } 
+    if (checkString == winString1 || checkString == winString2) {
+      alert('WIN!!!');
     } else {
-
-      dupe = true;
-      alert("That spot is taken! Choose somewhere else.")
+      checkString = "";
     }
-  } while ( dupe === true );
+  }
+
+  //First Diag
+  for (var i=0; i<board.length; i++) {
+    checkString = checkString + board[i][i];  //Wow easier than I thought
+  }
+  if (checkString == winString1 || checkString == winString2) {
+    alert('WIN!!!');
+  } else {
+    checkString = "";
+  }
+
+
+  //Second Diag
+  for (var i=0; i<board.length; i++) {
+    for (var k=board.length-1; k >= 0; k--) {   //Count down K
+      checkString = checkString + board[i][k];  
+    }
+    if (checkString == winString1 || checkString == winString2) {
+      alert('WIN!!!');
+    } else {
+      checkString = "";
+    }
+  }
 }
 
-//Initialise
-
-var state = true;
-// run the game on a loop
-while( state ) {
-  playerInput();
-  checkWin();
-  checkBoardFull();
-}
+generateBoard(2);
